6304bb37c43ab14f4677e41e7c9d9b54ea0f8bbf
Lucas <lucastkk@gmail.com>
2018-05-10 01:55:05 -0300

    added appicon, built launch image, added main view in autolayout, added constant file

---
+++ 10Closest/10Closest/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  10Closest
+//
+//  Created by Lucas Santos on 10/05/18.
+//  Copyright © 2018 Lucas Santos. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    /** INSERIR API KEY **/
+    static let GOOGLE_API_KEY = "AIzaSyCjV1x-ozxhmAF5-naoaBRj6nuw5DEN9VA"
+    /**************/
+    static let NEARBYSEARCH_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%@&radius=%@&keyword=%@&language=%@&rankby=distance&type=%@&key%@"
+    
+}
